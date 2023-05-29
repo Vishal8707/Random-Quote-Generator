333c635952334106cf188d89bd2426d208e0d4bd
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-05-29 20:57:04 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [quotes, setQuotes] = useState("");
+  const textRef = useRef();
+  let colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
+
+  const getQuotes = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomQuotes = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomQuotes]);
+      });
+  };
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  useEffect(() => {
+    textRef.current.style.color = colors[Math.floor(Math.random() * colors.length)] 
+  }, [quotes]);
+
+  return <div className="App">
+    <div className="quotes">
+      <p ref={textRef}>{quotes.text}</p>
+      <p>Author: {quotes.author}</p>
+      <div className="btnContainer">
+        <button onClick={getQuotes} className="btn" >Get Quotes</button>
+
+      </div>
     </div>
-  );
+  </div>;
 }
 
 export default App;
